965a69dc93ed00b41796fc3c8ada6804a2fe2e51
DozerLopez <victory1223@outlook.com>
2026-03-12 11:31:24 +0800

    Add PAPERS_DATA array with paper details

---
+++ registry.js
@@ -0,0 +1,16 @@
+const PAPERS_DATA = [
+    {
+        title: "算法时代的画笔",
+        author: "王平",
+        url: "papers/p2.html",
+        tag: "Digital",
+        date: "2024.10.27"
+    },
+    {
+        title: "论艺术体制的边界",
+        author: "匿名",
+        url: "papers/p1.html",
+        tag: "Theory",
+        date: "2024.10.25"
+    }
+];
